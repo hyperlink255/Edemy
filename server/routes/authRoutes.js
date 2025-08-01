84847ecea2b1592507db848a7b8fb199be54e479
Vikram Singh <your-email@example.com>
2025-08-01 22:03:18 +0530

    backend commited

--- server/routes/authRoutes.js
+++ server/routes/authRoutes.js
@@ -1,5 +1,10 @@
 import express from 'express'
-import { getUserData, purchaseCourse, userErolledCourses } from '../controllers/authController.js'
+import { addUserRating,
+     getUserCourseProgress,
+     getUserData, purchaseCourse,
+     updateUserCourseProgress,
+     userErolledCourses } 
+from '../controllers/authController.js'
 import { protect } from '../middleware/verifyToken.js'
 
 const router = express.Router()
@@ -7,6 +12,9 @@ const router = express.Router()
 router.get('/data',protect,getUserData)
 router.get('/enrolled-courses',protect,userErolledCourses)
 router.post('/purchase',protect,purchaseCourse)
+router.post('/update-course-progress',protect,updateUserCourseProgress)
+router.post('/get-course-progress',protect,getUserCourseProgress)
+router.post('/add-rating',protect,addUserRating)
 
 
 export default router
